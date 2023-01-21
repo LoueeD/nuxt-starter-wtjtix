f7e8e819a599acdd1e41d697b4f070e93824a7d8
LoueeD <loueed@gmail.com>
2023-01-21 22:21:15 +0000

    3 files modified

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,13 +1,5 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
   ssr: false,
-  buildModules: ['@nuxtjs/pwa'],
-  pwa: {
-    manifest: {
-      name: 'Demo App',
-      lang: 'gb',
-      useWebmanifestExtension: false,
-      theme_color: '#F6E8E3',
-    },
-  },
+  modules: ['@kevinmarrec/nuxt-pwa'],
 });
